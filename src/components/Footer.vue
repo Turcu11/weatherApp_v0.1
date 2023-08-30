b1662a3370eb4d8914346b3fd1273ce3d88c9aac
Turcu11 <iosif.turcu@emanuel.ro>
2023-08-30 22:28:32 +0300

    fixed the styles on the footer links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,12 +13,12 @@ import { RouterLink } from "vue-router";
         </div>
         <div class="footer-links">
             <div class="footer-author">
-                <Router-link to="/aboutTheAuthor">
+                <Router-link to="/aboutTheAuthor" class="footer-author-style">
                     About the author
                 </Router-link>
             </div>
             <div class="footer-github-link">
-                <Router-link to="/seeGithub">
+                <Router-link to="/seeGithub" class="footer-github-link-style">
                     See it on GitHub
                 </Router-link>
             </div>
@@ -51,18 +51,24 @@ import { RouterLink } from "vue-router";
     }
 }
 
-.footer-logo:hover {
-    text-shadow: rgba(0, 0, 0, 0.25) 0px 5px 15px;
-    border-bottom: 1px solid rgb(0, 189, 126);
-}
-
 .footer-links {
     display: flex;
     font-size: 0.9rem;
     gap: 2rem;
+    text-decoration: none;
 
     .footer-github-link {
         margin-right: 2rem;
+        .footer-github-link-style {
+            text-decoration: none;
+            color: rgb(76, 76, 76);
+        }
+    }
+    .footer-author{
+        .footer-author-style{
+            text-decoration: none;
+            color: rgb(76, 76, 76);
+        }
     }
 }
 </style>
